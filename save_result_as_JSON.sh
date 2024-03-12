9a70f1b70758aac8a74860751981f4cf427c8d0f
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2024-03-11 22:19:33 -0300

    Changed file 'save_result_as_JSON.sh': changed the error message when an invalid output file name is passed as argument; renamed key 'file_name' to 'file'.

--- save_result_as_JSON.sh
+++ save_result_as_JSON.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-    echo "Wrong input: please specify file name as argument. i.e: ./save_result.sh 'file_name'"
+    echo "Wrong input: please specify file name as argument. i.e: ./save_result_as_JSON.sh 'file_name'"
     exit 1
 fi
 
@@ -15,24 +15,27 @@ output_file=`echo "$output_file" | sed 's/ /-/g'`
 output_file=`echo "$output_file" | sed 's/\./-/g'`
 
 if [ -z "$output_file" ]; then
-    echo "Wrong input: please specify file name as argument. i.e: ./save_result.sh 'file_name'"
-    exit 1
+    echo -n "Wrong input: file name can use only letters, numbers,"
+    echo -n " _ and -; empty spaces and dots will be converted to -."
+    echo " i.e: ./save_result_as_JSON.sh 'file_name'"
+    exit 2
 fi
 
-date_time=`date '+%Y-%m-%d_%H-%M-%S'`
+date_time=`date '+%Y-%m-%d_%Hh%Mmin%Ss'`
 script_name=`basename "$0" | cut -d'.' -f1`
 output_file="${date_time}__${output_file}.json"
 
 file_content="["
-file_content+="{\"file_name\": \"listToRandomize.json\", \"content\": `cat listToRandomize.json`},"
-file_content+="{\"file_name\": \"lastResult.json\", \"content\": `cat lastResult.json`},"
-file_content+="{\"file_name\": \"alreadyRandomized.json\", \"content\": `cat alreadyRandomized.json`}"
+file_content+="{\"file\": \"listToRandomize.json\", \"content\": `cat listToRandomize.json`},"
+file_content+="{\"file\": \"lastResult.json\", \"content\": `cat lastResult.json`},"
+file_content+="{\"file\": \"alreadyRandomized.json\", \"content\": `cat alreadyRandomized.json`}"
 file_content+="]"
 
-echo -e "Saving file as: $output_file\n"
+echo -e "Saving file as: $output_file"
 echo "$file_content" > "$output_file"
 
 if command -v jq &> /dev/null; then
+    echo ''
     jq '' "./$output_file"
 
     tmp_content=`cat "$output_file" | jq '' -M`
