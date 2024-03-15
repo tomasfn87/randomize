c0d30880b4a645c0ca06ca3d9297e5f568f0df5b
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2024-03-15 19:20:17 -0300

    Changed file 'randomize.py': 1): added color to the partial result indication numbers to show a cycle is done by changing the color from yellow to green; 2): reorganized the program in general.

--- randomize.py
+++ randomize.py
@@ -1,19 +1,20 @@
-from datetime import datetime as dt
 from colorama import Fore, Style
+from datetime import datetime as dt
 import json
 import os
 import random
+import re
 import sys
 
 def read_json_file(file_path, default_content=None):
     if not os.path.exists(file_path) or os.stat(file_path).st_size == 0:
         return default_content
-    with open(file_path, 'r', encoding="utf-8") as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         data = json.load(file)
     return data
 
 def write_json_file(file_path, data):
-    with open(file_path, 'w') as file:
+    with open(file_path, "w") as file:
         json.dump(data, file, indent=2, ensure_ascii=False)
 
 def create_template_file(file_path, template_content):
@@ -24,25 +25,41 @@ def deduplicate(strings):
     unique_strings = set()
     deduplicated_list = []
     for string in strings:
-        normalized_string = ' '.join(string.lower().split())
+        normalized_string = re.sub(
+            r"[^a-zA-Z0-9À-ÿ]", "", " ".join(string.lower().split()))
         if normalized_string not in unique_strings:
             deduplicated_list.append(string)
             unique_strings.add(normalized_string)
     return deduplicated_list
+
+def join_text(item_list, separator_1=" and ", separator_2=", "):
+    assert isinstance(separator_1, str) and isinstance(separator_2, str)
+    text = ""
+    for i in range(0, len(item_list)):
+        text += str(item_list[i])
+        if i == len(item_list) - 1:
+            return text
+        elif i == len(item_list) - 2:
+            text += separator_1
+        else:
+            text += separator_2
 
 def print_color(text, color, end="\n"):
     if not type(text) == str:
         text = str(text)
 
     color_map = {
-        'yellow': Fore.YELLOW,
-        'red': Fore.RED,
-        'green': Fore.GREEN,
-        'light blue': Fore.CYAN,}
+        "dim": Style.DIM,
+        "yellow": Fore.YELLOW,
+        "red": Fore.LIGHTRED_EX,
+        "light yellow": Fore.LIGHTYELLOW_EX,
+        "light green": Fore.LIGHTGREEN_EX,
+        "light cyan": Fore.LIGHTCYAN_EX}
 
     if color not in color_map:
-        raise ValueError(
-            "Invalid color. Use 'yellow', 'red', 'green' or 'light blue'.")
+        err = "Invalid color. Use "
+        err += join_text(list(color_map.keys()), " or ") + "."
+        raise ValueError(err)
 
     print(color_map[color] + text + Style.RESET_ALL, end=end)
 
@@ -61,9 +78,22 @@ def randomizer(
     if options:
         random_result = random.choice(options)
         print(f"{description}:\n- ",  end="")
-        print_color(random_result, "light blue", end="")
+        if no_repeat and position == total_options:
+            print_color(random_result.strip(), "light green", end="")
+        else:
+            print_color(random_result.strip(), "light cyan", end="")
         if no_repeat:
-            print(f" ({position}/{total_options})", end="")
+            print(" (", end="")
+            if position == total_options:
+                print_color(position, "light green", end="")
+            else:
+                print_color(position, "yellow", end="")
+            print("/", end="")
+            if position == total_options:
+                print_color(total_options, "light green", end="")
+            else:
+                print_color(total_options, "light yellow", end="")
+            print(")", end="")
         print()
         if random_result is not None:
             return random_result
@@ -76,11 +106,14 @@ def main():
     if len(sys.argv) >= 2 and sys.argv[1].lower() == '--no-repeat':
         no_repeat = True
 
-    last_result_data = read_json_file('lastResult.json', default_content={})
+    last_result_data = read_json_file("lastResult.json", default_content={})
     list_to_randomize_data = read_json_file(
-        'listToRandomize.json', default_content={
+        "listToRandomize.json", default_content={
             "result_description": "my next trip will be to",
             "options": ["Portugal", "Spain", "France", "Italy"]})
+
+    result_description = list_to_randomize_data.get(
+        "result_description", "")
 
     options = list_to_randomize_data.get('options', [])
     check_options = deduplicate(options)
@@ -95,16 +128,23 @@ def main():
         print_color("ERROR", "red", end="")
         if len(options) == 1:
             print(": the only option is:", end=" ")
-            print_color(options[0], "light blue", end="")
+            print_color(options[0], "yellow", end="")
             print(".")
         else:
-            print(f": no options were defined.")
+            print(": no options were defined.")
         fix_file("listToRandomize.json")
+        return
+
+    if not result_description:
+        print_color("ERROR", "red", end="")
+        print(": invalid result description. Please check file ", end="")
+        print_color("listToRandomize.json", "yellow", end="")
+        print(".")
         return
 
     already_randomized_data = ""
     if no_repeat:
-        already_randomized_path = 'alreadyRandomized.json'
+        already_randomized_path = "alreadyRandomized.json"
 
         if os.path.exists(already_randomized_path):
             already_randomized_data = read_json_file(
@@ -121,9 +161,9 @@ def main():
 
         if already_randomized_data is not None:
             all_options_randomized_count = already_randomized_data.get(
-                'all_options_randomized_count', 0)
+                "all_options_randomized_count", 0)
             already_randomized = already_randomized_data.get(
-                'already_randomized', [])
+                "already_randomized", [])
 
             actual_options = []
             if already_randomized and already_randomized[0]["selected"]:
@@ -133,63 +173,59 @@ def main():
                 actual_options = []
 
         if len(already_randomized) < all_options_randomized_count + 1:
-            already_randomized_data['already_randomized'].insert(0, {
+            already_randomized_data["already_randomized"].insert(0, {
                 "datetime": str(dt.now())[0:19],
                 "comments": [], "selected": []})
-
-    result_description = list_to_randomize_data.get(
-        'result_description', None)
-
-    if result_description is None or not options:
-        print("Invalid configuration. Please check 'listToRandomize.json'.")
-        return
 
     total_options = len(options)
     if no_repeat and actual_options:
         options = [name for name in options if name not in actual_options]
 
-    last_result = last_result_data.get('last_result')
-    result_to_avoid = last_result
+    last_result = last_result_data.get("last_result")
 
     position = 0
     if no_repeat:
         position = len(
-            already_randomized_data['already_randomized'][0]["selected"]) + 1
+            already_randomized_data["already_randomized"][0]["selected"]) + 1
 
     new_result = randomizer(
-        options, result_to_avoid, result_description,
-        total_options, position, no_repeat)
+        options            = options,
+        result_to_avoid    = last_result,
+        result_description = result_description,
+        total_options      = total_options,
+        position           = position,
+        no_repeat          = no_repeat)
 
     if new_result is not None:
-        last_result_data['last_result'] = new_result
-        write_json_file('lastResult.json', last_result_data)
+        last_result_data["last_result"] = new_result
+        write_json_file("lastResult.json", last_result_data)
 
     if no_repeat:
-        already_randomized_data['already_randomized'][0]["selected"] \
+        already_randomized_data["already_randomized"][0]["selected"] \
             .append(new_result)
-        if len(already_randomized_data['already_randomized'][0]["selected"]) \
+        if len(already_randomized_data["already_randomized"][0]["selected"]) \
             >= total_options:
-            already_randomized_data['all_options_randomized_count'] = \
-                len(already_randomized_data['already_randomized'])
+            already_randomized_data["all_options_randomized_count"] = \
+                len(already_randomized_data["already_randomized"])
             all_options_randomized_count += 1
             write_json_file(
                 already_randomized_path, already_randomized_data)
 
-        print(
-            f"\nTimes list was completed: ", end="")
+        print_color(
+            "\nTimes list was completed:", "dim", end=" ")
         if all_options_randomized_count == 0:
             print_color(all_options_randomized_count, "red")
         else:
             if all_options_randomized_count % 2 == 0:
-                print_color(all_options_randomized_count, "yellow")
+                print_color(all_options_randomized_count, "light yellow")
             else:
-                print_color(all_options_randomized_count, "green")
+                print_color(all_options_randomized_count, "light green")
 
-        write_json_file('alreadyRandomized.json', already_randomized_data)
+        write_json_file("alreadyRandomized.json", already_randomized_data)
 
 if __name__ == "__main__":
-    create_template_file('lastResult.json', {})
-    create_template_file('listToRandomize.json', {
+    create_template_file("lastResult.json", {})
+    create_template_file("listToRandomize.json", {
         "result_description": "my next trip will be to",
         "options": ["Portugal", "Spain", "France", "Italy"]})
     main()
