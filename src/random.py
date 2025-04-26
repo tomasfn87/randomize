0ec197714f60052fe12726c2aca77a42167d4635
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2025-04-26 11:13:33 -0300

    Changed file 'src/random.py': the last result is now only avoided when there are more than 2 items to be randomized.

--- src/random.py
+++ src/random.py
@@ -1,5 +1,5 @@
 from tools.color import print_color
-from random import choice
+from random import choice # type: ignore
 
 class Random:
     def __init__(self, result_description, options, last_result):
@@ -11,42 +11,48 @@ class Random:
 
         description = self.result_description
 
-        options = [name for name in self.options if name != self.last_result]
-        if options:
-            random_result = choice(options)
-            print(f"{description}:\n- ",  end="")
-            if no_repeat and position == total_options:
-                if cycle_count % 2 == 0:
-                    print_color(random_result.strip(), "light green", end="")
-                else:
-                    print_color(random_result.strip(), "light yellow", end="")
-            elif cycle_count % 2 == 0:
-                print_color(
-                    random_result.strip(), "light cyan", end="")
-            else:
-                print_color(
-                    random_result.strip(), "light blue", end="")
-            if no_repeat:
-                print(" (", end="")
-                if position == total_options:
-                    if cycle_count % 2 == 0:
-                        print_color(position, "light green", end="")
-                    else:
-                        print_color(position, "light yellow", end="")
-                else:
-                    print_color(position, "dim", end="")
-                print("/", end="")
-                if position == total_options:
-                    if cycle_count % 2 == 0:
-                        print_color(total_options, "light green", end="")
-                    else:
-                        print_color(total_options, "light yellow", end="")
-                else:
-                    print(total_options, end="")
-                print(")", end="")
-            print()
-            if random_result is not None:
-                return random_result
+        if len(self.options) > 2:
+            options = [
+                name for name in self.options if name != self.last_result]
         else:
+            options = self.options
+
+        if not options:
             print("All options were selected. Let's start next round!")
             return None
+            
+        random_result = choice(options)
+        print(f"{description}:\n- ",  end="")
+        if no_repeat and position == total_options:
+            if cycle_count % 2 == 0:
+                print_color(random_result.strip(), "light green", end="")
+            else:
+                print_color(random_result.strip(), "light yellow", end="")
+        elif cycle_count % 2 == 0:
+            print_color(
+                random_result.strip(), "light cyan", end="")
+        else:
+            print_color(
+                random_result.strip(), "light blue", end="")
+        if no_repeat:
+            print(" (", end="")
+            if position == total_options:
+                if cycle_count % 2 == 0:
+                    print_color(position, "light green", end="")
+                else:
+                    print_color(position, "light yellow", end="")
+            else:
+                print_color(position, "dim", end="")
+            print("/", end="")
+            if position == total_options:
+                if cycle_count % 2 == 0:
+                    print_color(total_options, "light green", end="")
+                else:
+                    print_color(total_options, "light yellow", end="")
+            else:
+                print(total_options, end="")
+            print(")", end="")
+        print()
+        if random_result is not None:
+            return random_result
+ # type: ignore
